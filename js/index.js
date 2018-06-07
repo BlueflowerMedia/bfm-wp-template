f283c614b80a194a6b9f9788a32974411ccb23df
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-06-07 12:37:49 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,3 @@ var path = window.location.pathname;
 var page = path.split("/")
 var pageName = page[page.length - 2];
 
-
-if (pageName == "blueflowermedia") {
-  runParticles();
-  runCarousel();
-  runRellax();
-} else if (pageName == "team") {
-  teamImagePlayer();
-}
